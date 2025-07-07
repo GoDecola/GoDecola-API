77765b2ce9db12d7ca0bdd81395b996bb2a35e00
Letícia <leticiahcandido@gmail.com>
2025-07-07 16:50:19 -0300

    feat: endpoint cadastro de clientes

---
+++ GoDecola.API/Model/Clientes.cs
@@ -0,0 +1,33 @@
+﻿using GoDecola.API.ValidationAttribute;
+using System.ComponentModel.DataAnnotations;
+
+namespace GoDecola.API.Model
+{
+    public class Clientes
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        [MinLength(2, ErrorMessage = "O nome é inválido.")]
+        public string Nome { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "O e-mail é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O e-mail informado não é válido.")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "A senha é obrigatória.")]
+        [MinLength(8, ErrorMessage = "A senha deve ter no mínimo 8 caracteres.")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$",
+        ErrorMessage = "A senha deve conter pelo menos uma letra maiúscula, uma minúscula, um número e um caractere especial.")]
+        public string Senha { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "O telefone é obrigatório.")]
+        [RegularExpression(@"^\(?\d{2}\)? ?\d{4,5}-?\d{4}$", ErrorMessage = "Formato de telefone inválido.")]
+        public string Telefone { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "O CPF/CNPJ é obrigatório.")]
+        [CpfCnpj(ErrorMessage = "CPF ou CNPJ inválido.")]
+        public string Documento { get; set; } = string.Empty;
+
+    }
+}
