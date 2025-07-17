8de90af46ebc88231d207e60c24be196ff899f20
Gabriel Carvalho <gabrieldecb@gmail.com>
2025-07-17 16:40:08 -0300

    feat(repository): create User and TravelPackage repositories

---
+++ Repositories/TravelPackageRepository.cs
@@ -0,0 +1,54 @@
+﻿using GoDecola.API.Data;
+using GoDecola.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoDecola.API.Repositories
+{
+    public class TravelPackageRepository : IRepository<TravelPackage, int>
+    {
+        private readonly AppDbContext _context;
+
+        public TravelPackageRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<TravelPackage>> GetAllAsync()
+        {
+            return await _context.TravelPackages
+                .Include(tp => tp.Images) // inclui as imagens relacionadas
+                .Include(tp => tp.Videos) // inclui os vídeos relacionados
+                .ToListAsync(); // lista todos os pacotes de viagem
+        }
+
+        public async Task<TravelPackage?> GetByIdAsync(int id)
+        {
+            return await _context.TravelPackages
+                .Include(tp => tp.Images) // inclui as imagens relacionadas
+                .Include(tp => tp.Videos) // inclui os vídeos relacionados
+                .FirstOrDefaultAsync(tp => tp.Id == id); // busca pelo Id
+        }
+
+        public async Task AddAsync(TravelPackage travelPackage)
+        {
+            await _context.TravelPackages.AddAsync(travelPackage);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(TravelPackage travelPackage)
+        {
+            _context.TravelPackages.Update(travelPackage);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var travelPackage = await GetByIdAsync(id);
+            if (travelPackage != null)
+            {
+                _context.TravelPackages.Remove(travelPackage);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
