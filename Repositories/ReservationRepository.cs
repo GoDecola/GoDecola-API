1ac726a77e4b7ff2cba08d576d8cf52e799f0f1d
Gabriel Carvalho <gabrieldecb@gmail.com>
2025-07-21 19:30:56 -0300

    feat(repository): Add Reservation Repository

---
+++ Repositories/ReservationRepository.cs
@@ -0,0 +1,56 @@
+﻿using GoDecola.API.Data;
+using GoDecola.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoDecola.API.Repositories
+{
+    public class ReservationRepository : IRepository<Reservation, int>
+    {
+        private readonly AppDbContext _context;
+
+        public ReservationRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Reservation>> GetAllAsync()
+        {
+            return await _context.Reservations
+                .Include(r => r.User) // inclui o usuário relacionado
+                .Include(r => r.TravelPackage) // inclui o pacote de viagem relacionado
+                .Include(r => r.Guests) // inclui os hóspedes relacionados
+                .ToListAsync(); // lista todas as reservas
+        }
+
+        public async Task<Reservation?> GetByIdAsync(int id)
+        {
+            return await _context.Reservations
+                .Include(r => r.User) // inclui o usuário relacionado
+                .Include(r => r.TravelPackage) // inclui o pacote de viagem relacionado
+                .Include(r => r.Guests) // inclui os hóspedes relacionados
+                .FirstOrDefaultAsync(r => r.Id == id); // busca pela Id
+        }
+
+        public async Task AddAsync(Reservation reservation)
+        {
+            await _context.Reservations.AddAsync(reservation);
+            await _context.SaveChangesAsync();
+        }   
+
+        public async Task UpdateAsync(Reservation reservation)
+        {
+            _context.Reservations.Update(reservation);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Reservation reservation)
+        {
+            var existingReservation = await GetByIdAsync(reservation.Id);
+            if (existingReservation != null)
+            {
+                _context.Reservations.Remove(existingReservation);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
