654653fc2437dbe9bac475602a779110b08fbb2e
Gabriel Carvalho <gabrieldecb@gmail.com>
2025-07-21 16:45:16 -0300

    feat(automapper): create automapper

---
+++ Profiles/AutoMapperProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using GoDecola.API.Entities;
+using GoDecola.API.DTOs;
+using GoDecola.API.DTOs.UserDTOs;
+using GoDecola.API.DTOs.TravelPackageDTOs;
+using GoDecola.API.DTOs.ReservationDTOs;
+
+namespace GoDecola.API.Profiles
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserDTO>();
+            CreateMap<CreateUserDTO, User>();
+
+            CreateMap<TravelPackage, TravelPackageDTO>();
+            CreateMap<CreateTravelPackageDTO, TravelPackage>();
+
+            CreateMap<HotelAmenities, HotelAmenitiesDTO>();
+
+            CreateMap<Reservation, ReservationDTO>();
+            CreateMap<CreateReservationDTO, Reservation>();
+
+            CreateMap<Guests, GuestsDTO>();
+        }
+    }
+}
