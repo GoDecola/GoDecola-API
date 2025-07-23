7d3351c0cdb17de1daeff1f1e82c6b668d9eb06e
Gabriel Carvalho <gabrieldecb@gmail.com>
2025-07-23 19:21:33 -0300

    feat(TravelPackage): Add active status and promotion field

--- Entities/TravelPackage.cs
+++ Entities/TravelPackage.cs
@@ -1,4 +1,5 @@
 ﻿using GoDecola.API.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GoDecola.API.Entities
 {
@@ -11,13 +12,42 @@ namespace GoDecola.API.Entities
         public string? Destination { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
+        public bool IsActive { get; set; } = true;
         public int NumberGuests { get; set; }
         public AccommodationDetails AccommodationDetails { get; set; } = new AccommodationDetails();
         public ICollection<TravelPackageMedia> Medias { get; set; } = new List<TravelPackageMedia>();
         public PackageType PackageType { get; set; }
-
         public double? DiscountPercentage { get; set; } // ex: 0.10 para 10% de desconto
         public DateTime? PromotionStartDate { get; set; }
         public DateTime? PromotionEndDate { get; set; }
+
+        [NotMapped]
+        public bool IsCurrentlyOnPromotion
+        {
+            get
+            {
+                // se a varaivel DiscountPercentage nao estiver definida ou for menor ou igual a zero, nao esta em promo
+                if (!DiscountPercentage.HasValue || DiscountPercentage.Value <= 0)
+                {
+                    return false;
+                }
+
+                DateTime now = DateTime.UtcNow;
+
+                // se as datas de promocao nao estao definidas ou é maior que a data atual (invalido), nao esta em promocao
+                if (PromotionStartDate.HasValue && PromotionStartDate.Value > now)
+                {
+                    return false;
+                }
+
+                // se a data de promocao final estiver definida e for menor que a data atual (invalido), nao esta em promocao
+                if (PromotionEndDate.HasValue && PromotionEndDate.Value < now)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+        }
     }
 }
