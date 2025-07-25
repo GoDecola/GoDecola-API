4a67d9127ee298479cd7583de67d76c6ac75f133
Letícia <leticiahcandido@gmail.com>
2025-07-25 16:06:15 -0300

    feature: create payment dtos

---
+++ DTOs/PaymentDTOs/PaymentRequestDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace GoDecola.API.DTOs.PaymentDTOs
+{
+    public class PaymentRequestDTO
+    {
+        public int ReservationId { get; set; }  // id da reserva associada ao pagamento
+        public float Amount { get; set; }  // valor do pagamento
+        public string Method { get; set; } = string.Empty; // método de pagamento (cartão de crédito, pix, etc.))
+    }
+}
---
+++ DTOs/PaymentDTOs/PaymentResponseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace GoDecola.API.DTOs.PaymentDTOs
+{
+    public class PaymentResponseDTO
+    {
+        public string RedirectUrl { get; set; } = string.Empty; //url de redirecionamento para o pagamento
+        public string? Status { get; set; } // status do pagamento (pendente, confirmado, falha, cancelado)
+        public float? AmountPaid { get; set; } // valor pago
+        public DateTime? PaymentDate { get; set; } // data do pagamentoo
+    }
+}
