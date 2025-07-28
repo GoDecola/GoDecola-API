b336b4ea3042820cf7ceb5146a1396d4e7b4bedc
Letícia <leticiahcandido@gmail.com>
2025-07-28 17:27:50 -0300

    create payment controller

---
+++ Controllers/PaymentController.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using GoDecola.API.DTOs.PaymentDTOs;
+using GoDecola.API.DTOs.UserDTOs;
+using GoDecola.API.Entities;
+using GoDecola.API.Enums;
+using GoDecola.API.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoDecola.API.Controllers
+{
+    [ApiController]
+    [Route("api/payment")]
+    public class PaymentController : ControllerBase
+    {
+        private readonly IPaymentService _paymentService;
+
+        public PaymentController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+
+        [HttpPost("create-checkout-session")]
+        public async Task<IActionResult> CreateCheckoutSession([FromBody] PaymentRequestDTO request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                var response = await _paymentService.InitiateStripeCheckout(request);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                // log de erro opcional
+                return StatusCode(500, new { error = ex.Message });
+            }
+        }
+    }
+}
